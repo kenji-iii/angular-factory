3ca15bbde33d81493ed66d3ce601de930863a1d8
kenji <kenji.yoshioka.ac@gmail.com>
2018-07-02 21:49:34 +0900

    add angular flex

--- angular-flex-sample/src/app/app.module.ts
+++ angular-flex-sample/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
